a0b13ede16061316f58ee1b4fc08ca98f6030dc6
Joe Fiorini <joe@joefiorini.com>
2012-06-09 10:34:00 -0400

    Moves less files to own folder

--- server.js
+++ server.js
@@ -2,18 +2,27 @@ var bricks = require('bricks');
 var appServer = new bricks.appserver();
 var less = require('less');
 var fs = require('fs');
+var path = require('path');
 
-var lessPlugin = {
-  plugin: function(request, response){
-    var filename = __dirname + request.url + ".less";
-    var parser = new less.Parser;
+var lessPlugin = (function(){
+
+  var plugin = {},
+      basedir = "";
+
+  plugin.init = function(options){
+    basedir = path.join(__dirname, options.basedir);
+  }
+
+  plugin.plugin = function(request, response){
+    var filename = path.join(basedir, path.basename(request.url)) + ".less";
+    var parser = new(less.Parser)({ paths: [basedir] });
     fs.readFile(filename, 'utf-8', function(e, str){
       if(e && e.errno == 34){
         appServer.plugins.filehandler.plugin(request, response, {});
       } else {
         parser.parse(str, function(e, tree){
           if(e){
-            console.log("Error: ", e);
+            console.log("Error: ", JSON.stringify(e));
           }
           css = tree.toCSS({compress: false});
           response.setHeader("content-type", "text/css");
@@ -23,9 +32,11 @@ var lessPlugin = {
       }
     });
   }
-};
 
-appServer.addRoute("/css/.+", lessPlugin, { basedir: "./css/" });
+  return plugin;
+})();
+
+appServer.addRoute("/css/.+", lessPlugin, { basedir: "./less/" });
 appServer.addRoute("/.+", appServer.plugins.filehandler, { basedir: "./" });
 appServer.addRoute(".+", appServer.plugins.fourohfour);
 
